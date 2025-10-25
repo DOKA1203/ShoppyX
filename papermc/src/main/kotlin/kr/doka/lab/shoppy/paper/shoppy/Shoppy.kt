6ba70352a01f9627dc3fb55a107ab594160f323d
doka1203 <pomaseminecraft@gmail.com>
2025-10-25 13:06:10 +0900

    작?동

--- papermc/src/main/kotlin/kr/doka/lab/shoppy/paper/shoppy/Shoppy.kt
+++ papermc/src/main/kotlin/kr/doka/lab/shoppy/paper/shoppy/Shoppy.kt
@@ -8,6 +8,8 @@ import kr.doka.lab.shoppy.paper.database.ShopItems
 import kr.doka.lab.shoppy.paper.database.Shops
 import org.bukkit.entity.Player
 import org.bukkit.inventory.ItemStack
+import org.jetbrains.exposed.sql.SqlExpressionBuilder.eq
+import org.jetbrains.exposed.sql.deleteWhere
 import org.jetbrains.exposed.sql.insert
 import org.jetbrains.exposed.sql.insertAndGetId
 import org.jetbrains.exposed.sql.selectAll
@@ -73,15 +75,15 @@ class Shoppy(val name: String) {
                                 it[size] = 54
                             }
                     }
-
+                    ShopItems.deleteWhere { ShopItems.shop eq shopId?.value }
                     list.forEach { shoppyData ->
                         ShopItems.insert {
                             it[page] = shoppyData.page
                             it[shop] = shopId!!
                             it[slot] = shoppyData.id
                             it[itemStackBase64] = Base64.encode(shoppyData.item.serializeAsBytes())
-                            it[sellPrice] = shoppyData.sellPrice // 팔기 불가
-                            it[buyPrice] = shoppyData.buyPrice // 사기만 가능
+                            it[sellPrice] = shoppyData.sellPrice
+                            it[buyPrice] = shoppyData.buyPrice
                         }
                     }
                 }
